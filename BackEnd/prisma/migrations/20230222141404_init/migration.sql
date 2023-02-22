845257a84643874d4bbe5eeee72c1f30ae69a1ba
Vicafe04 <vinicius51885189@gmail.com>
2023-02-22 11:34:14 -0300

    atualizado

---
+++ BackEnd/prisma/migrations/20230222141404_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE `Motoristas` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `CNH` VARCHAR(191) NOT NULL,
+    `CPF` VARCHAR(191) NOT NULL,
+    `nome` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Servicos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `data_saida` DATETIME(3) NOT NULL,
+    `retorno` DATETIME(3) NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `motoristaId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Frotas` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `modelo` VARCHAR(191) NOT NULL,
+    `marca` VARCHAR(191) NOT NULL,
+    `placa` VARCHAR(191) NOT NULL,
+    `servicoId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Manutencao` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `valor` DOUBLE NOT NULL,
+    `descricao` VARCHAR(191) NOT NULL,
+    `data_inicio` VARCHAR(191) NOT NULL,
+    `data_fim` VARCHAR(191) NULL,
+    `frotaId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Servicos` ADD CONSTRAINT `Servicos_motoristaId_fkey` FOREIGN KEY (`motoristaId`) REFERENCES `Motoristas`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Frotas` ADD CONSTRAINT `Frotas_servicoId_fkey` FOREIGN KEY (`servicoId`) REFERENCES `Servicos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Manutencao` ADD CONSTRAINT `Manutencao_frotaId_fkey` FOREIGN KEY (`frotaId`) REFERENCES `Frotas`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
