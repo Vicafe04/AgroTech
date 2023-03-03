1ef85998579863ddbed124a0b6985d4e42fcbf16
Vicafe04 <vinicius51885189@gmail.com>
2023-03-03 10:22:51 -0300

    atualizado

--- FrontEnd/frotas/script.js
+++ FrontEnd/frotas/script.js
@@ -1,20 +1,36 @@
-const lista = document.querySelector(".list")
-const itemlista = document.querySelector(".item-list")
-
+const lista = document.querySelector(".list");
+const itemlista = document.querySelector(".item-list");
+const inpModel = document.querySelector("#inpMarca");
+const inpMarca = document.querySelector("#inpModelo");
+const inpPlaca = document.querySelector("#inpPlaca");
 
 function load() {
     fetch("http://localhost:3000/frota/read")
         .then(resp => { return resp.json() })
         .then(frota => {
-            console.log(frota)
 
             frota.forEach(e => {
                 let item = itemlista.cloneNode(true);
                 item.classList.remove("model");
-                item.querySelector(".marca").innerHTML = 'Marca: ' + frota.marca;
-                item.querySelector(".modelo").innerHTML = 'Modelo: ' + frota.modelo;
-                item.querySelector(".placa").innerHTML = 'Placa: ' + frota.placa;
+                item.querySelector(".marca").innerHTML = 'Marca: ' + e.marca;
+                item.querySelector(".modelo").innerHTML = 'Modelo: ' + e.modelo;
+                item.querySelector(".placa").innerHTML = 'Placa: ' + e.placa;
                 lista.appendChild(item);
             })
         })
+}
+
+function add() {
+    var dados = {
+        modelo: inpModel.value,
+        marca: inpMarca.value,
+        placa: inpPlaca.value
+    }
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+    };
+    fetch("http://localhost:3000/frota/read", options)
 }
