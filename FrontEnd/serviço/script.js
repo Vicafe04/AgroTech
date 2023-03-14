5fd39e6038211f698196b3b113aabb695a1b70be
Vicafe04 <vinicius51885189@gmail.com>
2023-03-14 11:37:50 -0300

    atualizado

--- FrontEnd/serviço/script.js
+++ FrontEnd/serviço/script.js
@@ -1,7 +1,12 @@
+const { options } = require("../../BackEnd/src/routes/router")
+
 const list = document.querySelector(".list")
 const itemList = document.querySelector(".item-list")
 const list2 = document.querySelector(".list2")
 const itemList2 = document.querySelector(".item-list2")
+
+const inpIdFunc = document.querySelector(".idFunc")
+const inpIdFrot = document.querySelector(".idFrot")
 
 fetch("http://localhost:3000/servico/read")
     .then(resp => { return resp.json() })
@@ -11,26 +16,35 @@ fetch("http://localhost:3000/servico/read")
 
 function load(data) {
     data.forEach(servicos => {
-        if ((servicos.Frota[0] ==    null) || (servicos.motorista[0] == null)) {
+        if ((servicos.Frota[0] == null) || (servicos.motorista[0] == null)) {
             let item = itemList2.cloneNode(true);
             item.classList.remove("model");
             item.querySelector(".imgDelivery").src = "../../Docs/img/delivery.png";
-            item.querySelector(".destino").innerHTML = "Destino:" + "<br/>" + servicos.descricao;
-            item.querySelector(".dataSaida").innerHTML = "Data de saida:" + "<br/>" + servicos.data_saida.split('T')[0];
-            item.querySelector(".dataRetorno").innerHTML = "Data de retorno:" + "<br/>" + servicos.retorno.split('T')[0];
+            item.querySelector(".id").innerHTML = "Id: " + servicos.id;
+            item.querySelector(".destino").innerHTML = "Destino: " + servicos.descricao;
+            item.querySelector(".dataSaida").innerHTML = "Data de saida: " + servicos.data_saida.split('T')[0];
+            item.querySelector(".dataRetorno").innerHTML = "Data de retorno: " + servicos.retorno.split('T')[0];
             list2.appendChild(item);
         } else {
             let item = itemList.cloneNode(true);
             item.classList.remove("model");
             item.querySelector(".imgDelivery").src = "../../Docs/img/delivery.png";
-            item.querySelector(".destino").innerHTML = "Destino:" + "<br/>" + servicos.descricao;
-            item.querySelector(".motorista").innerHTML = "Motorista:" + "<br/>" + servicos.motorista[0].nome;
-            item.querySelector(".veiculo").innerHTML = "Veiculo:" + "<br/>" + servicos.Frota[0].marca + servicos.Frota[0].modelo;
-            item.querySelector(".placa").innerHTML = "Placa:" + "<br/>" + servicos.Frota[0].placa;
-            item.querySelector(".dataSaida").innerHTML = "Data de saida:" + "<br/>" + servicos.data_saida.split('T')[0];
-            item.querySelector(".dataRetorno").innerHTML = "Data de retorno:" + "<br/>" + servicos.retorno.split('T')[0];
+            item.querySelector(".id").innerHTML = "Id: " + servicos.id;
+            item.querySelector(".destino").innerHTML = "Destino: " + servicos.descricao;
+            item.querySelector(".motorista").innerHTML = "Motorista: " + servicos.motorista[0].nome;
+            item.querySelector(".veiculo").innerHTML = "Veiculo: " + servicos.Frota[0].marca + servicos.Frota[0].modelo;
+            item.querySelector(".placa").innerHTML = "Placa: " + servicos.Frota[0].placa;
+            item.querySelector(".dataSaida").innerHTML = "Data de saida: " + servicos.data_saida.split('T')[0];
+            item.querySelector(".dataRetorno").innerHTML = "Data de retorno: " + servicos.retorno.split('T')[0];
             list.appendChild(item);
-            
+
         }
     })
+}
+
+function volunteer() {
+    const options={
+        
+    }
+    fetch("http://localhost:3000/motorista/update/" + inpIdFunc, options)
 }
